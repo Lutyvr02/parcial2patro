b9c22260362dd30d548297703ffc32a4396f346a
LucianoVargas <lutyvr02@gmail.com>
2023-03-08 20:00:49 -0400

    4rt commit

--- src/ej2/chateduca.java
+++ src/ej2/chateduca.java
@@ -5,19 +5,68 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class chateduca implements ican{
-    private Map<Integer,colega> ty = new HashMap<>();
-    private List<Integer> tyty = new ArrayList<>();
+public class chateduca implements ican {
+    private Map<Integer, colega> chatdegente = new HashMap<>();
+    private List<Integer> listadeid = new ArrayList<>();
     int llave;
 
     @Override
     public void enviar(String mensaje, colega col) {
-        colega ty1 = col;
-        for (int gente: tyty){
-            if(gente instanceof admin){
-                admin adty = (admin)ty1;
-                if (gente != admin)
+        colega origendelenvio = col;
+        for (int gente : listadeid) {
+            if (origendelenvio instanceof admin) {
+                admin adminis = (admin) origendelenvio;
+                if (gente != adminis.getId()) {
+                    chatdegente.get(gente).recibio(mensaje);
+                }
+            } else if (origendelenvio instanceof docente && (chatdegente.get(gente)
+                    instanceof docente) || chatdegente.get(gente) instanceof estudiante) {
+                docente doc = (docente) origendelenvio;
+                if (gente != doc.getCi()) {
+                    chatdegente.get(gente).recibio(mensaje);
+                }
+            } else if (origendelenvio instanceof estudiante && (chatdegente.get(gente)
+                    instanceof docente) || chatdegente.get(gente) instanceof admin) {
+                estudiante estu = (estudiante) origendelenvio;
+                if (gente != estu.getMatri()) {
+                    chatdegente.get(gente).recibio(mensaje);
+                } else if (gente != estu.getMatri() && estu.isEnviartodos()) {
+                    chatdegente.get(gente).recibio(mensaje);
+
+                } else if (gente != estu.getMatri() && estu.isEnviaraladmin() && chatdegente.get(gente) instanceof admin) {
+                    chatdegente.get(gente).recibio(mensaje);
+
+                } else if (gente != estu.getMatri()&& estu.isEnviaralprofe()&&chatdegente.get(gente) instanceof docente){
+                    chatdegente.get(gente).recibio(mensaje);
+                }
             }
         }
     }
+
+        public void anagente (colega anadirgente){
+            if (anadirgente instanceof estudiante) {
+                estudiante est = (estudiante) anadirgente;
+                llave = est.getMatri();
+                listadeid.add(llave);
+                anadiramapa(llave, anadirgente);
+            } else if (anadirgente instanceof docente) {
+                docente doc = (docente) anadirgente;
+                llave = doc.getCi();
+                listadeid.add(llave);
+                anadiramapa(llave, anadirgente);
+            }
+            if (anadirgente instanceof admin) {
+                admin ad = (admin) anadirgente;
+                llave = ad.getId();
+                listadeid.add(llave);
+                anadiramapa(llave, anadirgente);
+
+            }
+        }
+
+        public void anadiramapa ( int idperso, colega persona){
+            chatdegente.put(idperso, persona);
+        }
+
 }
+
--- src/ej2/client.java
+++ src/ej2/client.java
@@ -1,4 +1,37 @@
 package ej2;
 
 public class client {
+    public static void main(String[] args) {
+        chateduca cha = new chateduca();
+        estudiante est1 = new estudiante(cha, "eadasdasd",312312321,true,false,false);
+        estudiante est2 = new estudiante(cha, "eadasdasd",312312321,true,true,false);
+        estudiante est3 = new estudiante(cha, "eadasdasd",312312321,true,true,false);
+        estudiante est4 = new estudiante(cha, "eadasdasd",312312321,true,false,false);
+        estudiante est5 = new estudiante(cha, "eadasdasd",312312321,true,false,true);
+
+        admin ad1 = new admin(cha,"dasdasdsa","asdasdsad",545345);
+        admin ad2 = new admin(cha,"dfdafasfas","hdfgdfhdf",5345345);
+        admin ad3 = new admin(cha,"fasfasfasf","kukyukuykuy",1231231);
+
+        docente doc1 = new docente(cha, "erqedadasd","asdasdasda",2312312);
+        docente doc2 = new docente(cha, "efasdfasfas","jfghfghgfh",454325324);
+        docente doc3 = new docente(cha, "bcvbcvb","cvbcvbcv",345345435);
+
+        cha.anagente(est1 );
+        cha.anagente(est2 );
+        cha.anagente(est3 );
+        cha.anagente(est4 );
+        cha.anagente(ad2 );
+        cha.anagente(ad1 );
+        cha.anagente(ad3 );
+        cha.anagente(doc1 );
+        cha.anagente(doc2 );
+        cha.anagente(doc3 );
+
+        est1.enviar("hola docente");
+        est1.enviar("hola todos");
+        est1.enviar("hola admin");
+        est1.enviar("hola docente y estudiantes");
+        est1.enviar("hola a todos");
+    }
 }
